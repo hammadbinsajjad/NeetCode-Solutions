ed2aa52817331e734a3a80af2f26aabd6b6ca0dc
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2023-08-03 20:49:06 +0500

    Fixed House Robber Solution

--- 1-D Dynamic Programming/House Robber/solution.py
+++ 1-D Dynamic Programming/House Robber/solution.py
@@ -3,6 +3,13 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         for i in range(2, len(nums)):
-            nums[i] = max(nums[i - 1], nums[i] + nums[i - 2])
+            if i == 2:
+                nums[i] = max(nums[i - 1], nums[i] + nums[i - 2])
+            else:
+                nums[i] = max(nums[i - 1], nums[i] + nums[i - 2], nums[i] + nums[i - 3])
+
+
+        if len(nums) == 2:
+            return max(nums)
 
         return nums[-1]
